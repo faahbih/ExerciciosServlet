3a02955cc01df93f6c2bbac145b214d9bccf548d
faahbih <fabiana.rr.oliveira@gmail.com>
2016-05-25 20:04:15 -0300

    Create ShowRequestHeaders.java

---
+++ ShowRequestHeaders.java
@@ -0,0 +1,53 @@
+6. Escreva uma servlet que leia e imprima os cabeçalhos da informação HTTP que é enviada do cliente para o servidor.
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowRequestHeaders
+ */
+@WebServlet("/ShowRequestHeaders")
+public class ShowRequestHeaders extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String title = "Servlet Example: Showing Request Headers";
+		out.println(ServletUtilities.headWithTitle(title) +
+				"<BODY BGCOLOR=\"#FDF5E6\">\n" +
+				"<H1 ALIGN=\"CENTER\">" + title + "</H1>\n" +
+				"<B>Request Method: </B>" +
+				request.getMethod() + "<BR>\n" +
+				"<B>Request URI: </B>" +
+				request.getRequestURI() + "<BR>\n" +
+				"<B>Request Protocol: </B>" +
+				request.getProtocol() + "<BR><BR>\n" +
+				"<TABLE BORDER=1 ALIGN=\"CENTER\">\n" +
+				"<TR BGCOLOR=\"#FFAD00\">\n" +
+				"<TH>Header Name<TH>Header Value");
+		Enumeration headerNames = request.getHeaderNames();
+		while(headerNames.hasMoreElements()) {
+			String headerName = (String)headerNames.nextElement();
+			out.println("<TR><TD>" + headerName);
+			out.println(" <TD>" + request.getHeader(headerName));
+		}
+		out.println("</TABLE>\n</BODY></HTML>");
+	}
+	/** Let the same servlet handle both GET and POST. */
+	public void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
